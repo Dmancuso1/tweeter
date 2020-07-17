a9d26b4a29a52e24df0a5fb0c0d56bbe83820cf3
Dane Mancuso <danegmancuso@gmail.com>
2020-07-17 02:28:58 +0000

    updated autofocus helper function

--- public/scripts/autofocus-helper.js
+++ public/scripts/autofocus-helper.js
@@ -1,9 +1,15 @@
 // autofocus on small screens. 
 // TODO: fix so not have to refresh page to run this function..
 $(document).ready(function() {
+
   const textBox = $("#tweet-text");
   if ($( window ).width() < 620) {
     textBox.focus()
   }
-});
 
+
+  $('#write-new-tweet').on('click', function() {
+    textBox.focus()
+  })
+
+});
--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
